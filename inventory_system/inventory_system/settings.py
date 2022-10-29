5b41fb4ebd19fcf2e43abf024e4fe95ef6efbc79
muhammadzaim <muhammadzaim@wavelet.net>
2022-10-29 16:09:09 +0800

    Init api

--- inventory_system/inventory_system/settings.py
+++ inventory_system/inventory_system/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,8 @@ STATICFILES_DIRS = [BASE_DIR/ "inventory_system/static"]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
